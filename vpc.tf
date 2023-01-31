cf802b6997570da2575e2ef4a540048c118dcc47
DinaraKA <d.karybekova@gmail.com>
2023-01-31 00:25:26 -0500

    change vpc file

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,48 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-  name = var.vpc_name
-  cidr = var.vpc_cidr
+  tags = {
+    Name = "MyVPC"
+  }
+}
 
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+  tags = {
+    Name = "MyIG"
+  }
+}
 
-  tags = var.tags
+resource "aws_route" "main_route" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Subnet2"
+  }
+}
+
+resource "aws_subnet" "subnet3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "Subnet3"
+  }
 }
